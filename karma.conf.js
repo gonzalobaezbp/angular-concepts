570c320ce6f52b38592b1e300e248dcaeb7bb91e
gonzalobaezbp <gonzalobaezbp@gmail.com>
2021-10-12 10:02:40 -0500

    rename of folder project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-pokemon'),
+      dir: require('path').join(__dirname, './coverage/angular-concepts'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/prueba'));
+app.use(express.static('./dist/angular-concepts'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/prueba/' }),
+    res.sendFile('index.html', { root: './dist/angular-concepts' }),
 );
 
 // Start the app by listening on the default Heroku port
